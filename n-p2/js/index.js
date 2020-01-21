a06ffbcce10fe5c2601698af5027aa75c13cf6e0
karimcoda <40443372+karimcoda@users.noreply.github.com>
2020-01-21 22:57:44 +0100

    Add files via upload

---
+++ n-p2/js/index.js
@@ -0,0 +1,463 @@
+$(document).ready(function(){
+  //button 1
+  $('button.colorb-1').click(function(){
+    $('header').css({
+      "background-color": "#eb4034",
+      "color": "#000"
+    })
+    $('ul.menu li a').css({
+      "color": "#000"
+    });
+    
+    $('ul.menu li.element1').mouseover(function() {
+      $('ul.menu li a.link1').css({
+        "background-color": "#000",
+        "color": "#fff"
+      })
+      
+    });
+    $('ul.menu li.element2').mouseover(function() {
+      $('ul.menu li a.link2').css({
+        "background-color": "#000",
+        "color": "#fff"
+      })
+      
+    });
+    $('ul.menu li.element3').mouseover(function() {
+      $('ul.menu li a.link3').css({
+        "background-color": "#000",
+        "color": "#fff"
+      })
+      
+    });
+    
+    $('ul.menu li a').mouseout(function() {
+      $(this).css("background-color","transparent")
+      $('a').css("color","#000")
+    });
+    $('#section-a').css("background" ,"rgb(249, 200, 196)");
+    $('#section-c').css("background" ,"rgb(249, 200, 196)");
+    $('#section-c img').css("border" ,"rgb(249, 200, 196) 4px solid");
+
+    $('li.lie1').mouseover(function() {
+      $(this).css({
+        "background-color": "#eb4034"
+
+        
+      })
+      $('a.aie1').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie2').mouseover(function() {
+      $(this).css({
+        "background-color": "#eb4034"
+
+        
+      })
+      $('a.aie2').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie3').mouseover(function() {
+      $(this).css({
+        "background-color": "#eb4034"
+
+        
+      })
+      $('a.aie3').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie4').mouseover(function() {
+      $(this).css({
+        "background-color": "#eb4034"
+
+        
+      })
+      $('a.aie4').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li').mouseout(function() {
+      $(this).css("background-color","transparent")
+      $('a').css("color","#000")
+    });
+    
+  }); 
+  //--------------------------------------
+//button 2
+
+  $('button.colorb-2').click(function(){
+    $('header').css({
+      "background-color": "#cdeb34",
+      "color": "#000"
+    })
+    $('ul.menu li a').css({
+      "color": "#000"
+    });
+    
+    $('ul.menu li.element1').mouseover(function() {
+      $('ul.menu li a.link1').css({
+        "background-color": "#000",
+        "color": "#fff"
+      })
+      
+    });
+    $('ul.menu li.element2').mouseover(function() {
+      $('ul.menu li a.link2').css({
+        "background-color": "#000",
+        "color": "#fff"
+      })
+      
+    });
+    $('ul.menu li.element3').mouseover(function() {
+      $('ul.menu li a.link3').css({
+        "background-color": "#000",
+        "color": "#fff"
+      })
+      
+    });
+    
+    $('ul.menu li a').mouseout(function() {
+      $(this).css("background-color","transparent")
+      $('a').css("color","#000")
+    });
+    $('#section-a').css("background" ,"#f1ffaa");
+    $('#section-c').css("background" ,"#f1ffaa");
+    $('#section-c img').css("border" ,"#f1ffaa 4px solid");
+
+    $('li.lie1').mouseover(function() {
+      $(this).css({
+        "background-color": "#cdeb34"
+
+        
+      })
+      $('a.aie1').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie2').mouseover(function() {
+      $(this).css({
+        "background-color": "#cdeb34"
+
+        
+      })
+      $('a.aie2').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie3').mouseover(function() {
+      $(this).css({
+        "background-color": "#cdeb34"
+
+        
+      })
+      $('a.aie3').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie4').mouseover(function() {
+      $(this).css({
+        "background-color": "#cdeb34"
+
+        
+      })
+      $('a.aie4').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li').mouseout(function() {
+      $(this).css("background-color","transparent")
+      $('a').css("color","#000")
+    });
+    
+  }); 
+  });
+
+  //---------------------------
+  //button 3
+  $('button.colorb-3').click(function(){
+    $('header').css({
+      "background-color": "#34c3eb",
+      "color": "#000"
+    })
+    $('ul.menu li a').css({
+      "color": "#000"
+    });
+    
+    $('ul.menu li.element1').mouseover(function() {
+      $('ul.menu li a.link1').css({
+        "background-color": "#000",
+        "color": "#fff"
+      })
+      
+    });
+    $('ul.menu li.element2').mouseover(function() {
+      $('ul.menu li a.link2').css({
+        "background-color": "#000",
+        "color": "#fff"
+      })
+      
+    });
+    $('ul.menu li.element3').mouseover(function() {
+      $('ul.menu li a.link3').css({
+        "background-color": "#000",
+        "color": "#fff"
+      })
+      
+    });
+    
+    $('ul.menu li a').mouseout(function() {
+      $(this).css("background-color","transparent")
+      $('a').css("color","#000")
+    });
+    $('#section-a').css("background" ,"#b1eeff");
+    $('#section-c').css("background" ,"#b1eeff");
+    $('#section-c img').css("border" ,"#b1eeff 4px solid");
+
+    $('li.lie1').mouseover(function() {
+      $(this).css({
+        "background-color": "#34c3eb"
+
+        
+      })
+      $('a.aie1').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie2').mouseover(function() {
+      $(this).css({
+        "background-color": "#34c3eb"
+
+        
+      })
+      $('a.aie2').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie3').mouseover(function() {
+      $(this).css({
+        "background-color": "#34c3eb"
+
+        
+      })
+      $('a.aie3').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie4').mouseover(function() {
+      $(this).css({
+        "background-color": "#34c3eb"
+
+        
+      })
+      $('a.aie4').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li').mouseout(function() {
+      $(this).css("background-color","transparent")
+      $('a').css("color","#000")
+    });
+    
+  }); 
+
+  //--------------------------------------
+  //button 4
+
+  $('button.colorb-4').click(function(){
+    $('header').css({
+      "background-color": "#34eb43",
+      "color": "#000"
+    })
+    $('ul.menu li a').css({
+      "color": "#000"
+    });
+    
+    $('ul.menu li.element1').mouseover(function() {
+      $('ul.menu li a.link1').css({
+        "background-color": "#000",
+        "color": "#fff"
+      })
+      
+    });
+    $('ul.menu li.element2').mouseover(function() {
+      $('ul.menu li a.link2').css({
+        "background-color": "#000",
+        "color": "#fff"
+      })
+      
+    });
+    $('ul.menu li.element3').mouseover(function() {
+      $('ul.menu li a.link3').css({
+        "background-color": "#000",
+        "color": "#fff"
+      })
+      
+    });
+    
+    $('ul.menu li a').mouseout(function() {
+      $(this).css("background-color","transparent")
+      $('a').css("color","#000")
+    });
+    $('#section-a').css("background" ,"#92fd9b");
+    $('#section-c').css("background" ,"#92fd9b");
+    $('#section-c img').css("border" ,"#92fd9b 4px solid");
+
+    $('li.lie1').mouseover(function() {
+      $(this).css({
+        "background-color": "#34eb43"
+
+        
+      })
+      $('a.aie1').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie2').mouseover(function() {
+      $(this).css({
+        "background-color": "#34eb43"
+
+        
+      })
+      $('a.aie2').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie3').mouseover(function() {
+      $(this).css({
+        "background-color": "#34eb43"
+
+        
+      })
+      $('a.aie3').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie4').mouseover(function() {
+      $(this).css({
+        "background-color": "#34eb43"
+
+        
+      })
+      $('a.aie4').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li').mouseout(function() {
+      $(this).css("background-color","transparent")
+      $('a').css("color","#000")
+    });
+    
+  }); 
+  //------------------------------------------
+  //button 5 return
+
+  $('button.return').click(function(){
+    $('header').css({
+      "background-color": "#333",
+      "color": "#fff"
+    })
+    $('ul.menu li a').css({
+      "color": "#fff"
+    });
+    
+    $('ul.menu li.element1').mouseover(function() {
+      $('ul.menu li a.link1').css({
+        "background-color": "#e4ddd3",
+        "color": "#000"
+      })
+      
+    });
+    $('ul.menu li.element2').mouseover(function() {
+      $('ul.menu li a.link2').css({
+        "background-color": "#e4ddd3",
+        "color": "#000"
+      })
+      
+    });
+    $('ul.menu li.element3').mouseover(function() {
+      $('ul.menu li a.link3').css({
+        "background-color": "#e4ddd3",
+        "color": "#000"
+      })
+      
+    });
+    
+    $('ul.menu li a').mouseout(function() {
+      $(this).css("background-color","transparent")
+      $('a').css("color","#fff")
+    });
+    $('#section-a').css("background" ,"#e4ddd3");
+    $('#section-c').css("background" ,"#e4ddd3");
+    $('#section-c img').css("border" ,"#e4ddd3 4px solid");
+
+    $('li.lie1').mouseover(function() {
+      $(this).css({
+        "background-color": "#333"
+
+        
+      })
+      $('a.aie1').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie2').mouseover(function() {
+      $(this).css({
+        "background-color": "#333"
+
+        
+      })
+      $('a.aie2').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie3').mouseover(function() {
+      $(this).css({
+        "background-color": "#333"
+
+        
+      })
+      $('a.aie3').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li.lie4').mouseover(function() {
+      $(this).css({
+        "background-color": "#333"
+
+        
+      })
+      $('a.aie4').css({
+        "color": "#fff"
+      })
+      
+    });
+    $('li').mouseout(function() {
+      $(this).css("background-color","transparent")
+      $('a').css("color","#fff")
+    });
+    
+  }); 
+
+
